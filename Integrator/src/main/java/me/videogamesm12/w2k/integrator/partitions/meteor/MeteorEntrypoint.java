419dec26e65ef22d693f9aa4d99e1e86b3d15ed9
Video <videogamesm12@gmail.com>
2024-07-27 13:14:30 -0600

    Adds Meteor support

---
+++ Integrator/src/main/java/me/videogamesm12/w2k/integrator/partitions/meteor/MeteorEntrypoint.java
@@ -0,0 +1,34 @@
+package me.videogamesm12.w2k.integrator.partitions.meteor;
+
+import me.videogamesm12.w2k.blackbox.window.menu.W2KMenu;
+import me.videogamesm12.w2k.integrator.core.gui.PModCategoryMenu;
+import me.videogamesm12.w2k.integrator.core.gui.PModMenu;
+import meteordevelopment.meteorclient.MeteorClient;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+
+public class MeteorEntrypoint extends MeteorAddon
+{
+    @Override
+    public void onInitialize()
+    {
+        // Create our menu
+        final PModMenu<MeteorClient> menu = new PModMenu<>("Meteor Client", MeteorClient.INSTANCE);
+
+        // Loops through the categories in memory
+        Modules.loopCategories().forEach(category ->
+        {
+            final PModCategoryMenu categoryMenu = new PModCategoryMenu(category.name);
+            Modules.get().getGroup(category).forEach(module -> categoryMenu.addModule(new MeteorModuleMenu(module)));
+            menu.addSubMenu(categoryMenu);
+        });
+
+        // Adds the menu
+        W2KMenu.queueModMenu(menu);
+    }
+
+    public String getPackage()
+    {
+        return null;
+    }
+}
