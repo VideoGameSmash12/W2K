0e6c24098c530c944ee42a1a1308f8c8adeaae40
Video <videogamesm12@gmail.com>
2024-12-13 14:48:06 -0700

    Updates /w2k usage

--- src/main/java/me/videogamesm12/w2k/kernel/commands/W2KCmd.java
+++ src/main/java/me/videogamesm12/w2k/kernel/commands/W2KCmd.java
@@ -10,7 +10,7 @@ import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 
-@Parameters(name = "w2k", usage = "/<command> [details | experiments]")
+@Parameters(name = "w2k", usage = "/<command> [details]")
 public class W2KCmd extends WCommand
 {
     @Override
